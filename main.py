4e0fdbdc4e33c816711da47381f62ff240776135
LeonFerreirax <leon_stm@hotmail.com>
2020-04-03 15:08:09 -0300

    update main

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import cv2
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import mpegCoder
 from djitellopy.tello import Tello
 import pygame
 from alexnet import AlexNet
